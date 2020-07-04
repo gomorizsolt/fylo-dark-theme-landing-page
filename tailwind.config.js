4d6b0d398517ebd76a8140ae64b656f2a58fa62c
Zsolt Gömöri <zsgomoridev@icloud.com>
2020-07-04 11:38:40 +0200

    Add main content.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,13 @@ module.exports = {
     extend: {
       colors: {
         white: "hsl(0, 0%, 100%)",
+        cyan: "hsl(176, 68%, 64%)",
       },
       backgroundColor: {
         header: "hsl(217, 28%, 15%)",
         main: "hsl(218, 28%, 13%)",
-        footer: "hsl(219, 30%, 18%)",
+        footer: "hsl(216, 53%, 9%)",
+        testimonial: "hsl(219, 30%, 18%)",
       },
       fontFamily: {
         raleway: ["Raleway", "sans-serif"],
@@ -16,6 +18,12 @@ module.exports = {
       },
       minHeight: {
         header: "95vh",
+      },
+      maxWidth: {
+        testimonial: "30px",
+      },
+      fontSize: {
+        "2xs": ".6rem",
       },
     },
   },
