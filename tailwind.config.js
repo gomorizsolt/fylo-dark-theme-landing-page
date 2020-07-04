4a9279a8b6ba71138ebd5af0c75235e5875ae17f
Zsolt Gömöri <zsgomoridev@icloud.com>
2020-07-04 09:49:17 +0200

    Add header styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   purge: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+      },
+      backgroundColor: {
+        header: "hsl(217, 28%, 15%)",
+        main: "hsl(218, 28%, 13%)",
+        footer: "hsl(219, 30%, 18%)",
+      },
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+        "open-sans": ['"Open Sans", "sans-serif'],
+      },
+      minHeight: {
+        header: "95vh",
+      },
+    },
   },
   variants: {},
   plugins: [],
